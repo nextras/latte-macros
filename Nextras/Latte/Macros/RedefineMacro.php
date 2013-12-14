e475777f7bffc3d23b9c3e82a8d8e15c632d6180
hrach <hrach.cz@gmail.com>
2013-12-14 20:18:48 +0100

    Added phpdoc

--- Nextras/Latte/Macros/RedefineMacro.php
+++ Nextras/Latte/Macros/RedefineMacro.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * This file is part of the Nextras community extensions of Nette Framework
+ *
+ * @license    MIT
+ * @link       https://github.com/nextras/latte-macros
+ * @author     Jan Skrasek
+ */
+
 namespace Nextras\Latte\Macros;
 
 use Nette\Latte\CompileException;
